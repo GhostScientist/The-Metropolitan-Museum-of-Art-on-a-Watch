d2ca8ecfc53d055adb8ac8e489c10c92ee3eb08a
Dakota Kim <codingvampire@gmail.com>
2024-04-01 21:12:41 -0400

    Initial Commit

---
+++ The Metropolitan Museum of Art on a Watch Watch App/The_Metropolitan_Museum_of_Art_on_a_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Metropolitan_Museum_of_Art_on_a_WatchApp.swift
+//  The Metropolitan Museum of Art on a Watch Watch App
+//
+//  Created by Dakota Kim on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct The_Metropolitan_Museum_of_Art_on_a_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
